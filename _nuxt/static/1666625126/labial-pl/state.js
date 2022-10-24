efd3e50a52ace7745ec57011f68ba0f20db0828f
juanmarinelli <juanmarinelli10@gmail.com>
2022-10-24 12:26:13 -0300

    saddd

---
+++ _nuxt/static/1666625126/labial-pl/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1666625126",layout:"default",error:a,state:{productos:[],categories:[],cart:{cart:[],showCart:false},user:{users:[],user:a}},serverRendered:true,routePath:"\u002Flabial-pl",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
